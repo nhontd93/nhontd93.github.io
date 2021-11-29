27ac7376ba489c49c58b7118fdf86c3d19e2413f
nhontd93 <94519895+nhontd93@users.noreply.github.com>
2021-11-29 17:25:22 +0700

    Add files via upload

--- game.js
+++ game.js
@@ -11,20 +11,8 @@ class Game extends Node {
 }
     document.body.style.backgroundImage = "url(./images/cover.jpeg)";
     document.body.style.backgroundSize = "cover";
-    const images = [
-    "agasa.jpeg",
-    "ayumi.jpeg",
-    "conan.jpeg",
-    "genta.jpeg",
-    "haibara.jpeg",
-    "heiji.jpeg",
-    "mitsuhiko.jpeg",
-    "mori.jpeg",
-    "ran.jpeg",
-    "yusaku.jpeg"];
-    const duplicateImages = images.concat(images);
-    let imgRandom = [...duplicateImages];
     let imgSources = [];
+    let arrCovers = [];
     const startX = 10;
     const startY = 120;
     const col = 5;
@@ -32,43 +20,70 @@ class Game extends Node {
     let score = new Label();
     score.initScoreBox();
     let num = 0;
-    function createCards() {
+    function createCards(name) {
+      if (name == "REPLAY") {
+        detroyCard();
+        imgSources = [];
+      } else if (name == "RETRY") {
+        detroyCard();
+      }
+      const images = [
+        "agasa.jpeg",
+        "ayumi.jpeg",
+        "conan.jpeg",
+        "genta.jpeg",
+        "haibara.jpeg",
+        "heiji.jpeg",
+        "mitsuhiko.jpeg",
+        "mori.jpeg",
+        "ran.jpeg",
+        "yusaku.jpeg"];
+        const duplicateImages = images.concat(images);
+        let imgRandom = [...duplicateImages];
       for(let i=0 ; i<row; i++){
           for(let j = 0; j <col; j++){
-              let label = new Label();
-              let sprite = new Sprite();
-              let cover = new Cover();
+              var label = new Label();
+              var sprite = new Sprite();
+              var cover = new Cover();
               cover.initStyle();
               cover.addChild(label);
               cover.addChild(sprite);
+              arrCovers.push(cover.view);
+              imagesSource(name);
               cover.width = 100
               cover.height = 100;
               cover.x = 210;
               cover.y = 270; 
               sprite.width = 100;
               sprite.height = 100;
-              let ramdom = Math.floor(Math.random()*imgRandom.length);
-              sprite.setImage(`./images/${imgRandom[ramdom]}`);
-              document.body.appendChild(cover.view);
-              imgSources.push(imgRandom[ramdom]);                 
-              imgRandom.splice(ramdom,1);              
+              document.body.appendChild(cover.view);   
               num++;
               label.string = num;
               if (num > 19) num = 0;
-                      //Animation
               let tl = gsap.timeline({ repeat: 0, repeatDelay: 0 });
               tl.delay(0.05*num).to(cover, {
                   duration: 1.5,
                   ease: "elastic.out(0.5, 0.3)",
                   x: 100*j + startX + j,
                   y: 100*i + startY + i,                 
-              }); 
-           
+              });          
           }     
+      }
+      function imagesSource(name) {
+        if (name == "PLAY" || name == "REPLAY") {
+          let ramdom = Math.floor(Math.random()*imgRandom.length);
+              sprite.setImage(`./images/${imgRandom[ramdom]}`);
+              imgSources.push(imgRandom[ramdom]);                 
+              imgRandom.splice(ramdom,1);
+        } else {
+              sprite.setImage(`./images/${imgSources[num]}`);
+        }
+          
       }
       const covers = document.querySelectorAll('.cover');
       covers.forEach(cover => cover.addEventListener('click', flipCard));
   }   
+
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
@@ -119,15 +134,7 @@ scoreDOM.value = `SCORE: ${point}`;
       setTimeout(() => {
             if (isWin == 10) alert('YOU WIN! SCORE = ' + point);},500);
   }
-  function zoomCard(card) {
-      let tl = gsap.timeline({ repeat: 0, repeatDelay: 0 });
-        tl.add(function () {
-        card.firstChild.style.display = 'none';
-        card.lastChild.hidden = false;
-      }) ;
-      tl.to(card, { scale: 1.2, duration: 0.5 });
-  }
-
+  
   function unflipCards() {
         lockBoard = true;
         setTimeout(() => {
@@ -141,8 +148,7 @@ scoreDOM.value = `SCORE: ${point}`;
         } else {      
           scoreDOM.value = "SCORE: " + 0;
           setTimeout(() => {
-          alert("GAME OVER");
-          window.location = './index.html';},100);
+          alert("GAME OVER");},100);
         };
         },900); 
       setTimeout(() => {
@@ -158,54 +164,64 @@ scoreDOM.value = `SCORE: ${point}`;
   }
   
   let stylePlay = {
-    position: "absolute",
-    width: 100,
-    height: 50,
-    fontSize: "20px",
-    x: 10,
-    y: 30,
-    name: "PLAY"
+      position: "absolute",
+      width: 150,
+      height: 50,
+      fontSize: "20px",
+      x: 186,
+      y: 30,
+      name: "PLAY"
   }
   let btnPlay = new Button();
       btnPlay._initStyleElement(stylePlay);
       btnPlay.view.onclick = function () {
-        createCards();
+        createCards("PLAY");
         setTimeout(() => {
           score.view.hidden = false;
-          btnPlay.view.hidden = true;},3000);
+          btnPlay.view.hidden = true;
+          btnRetry.view.hidden = false;
+          btnReplay.view.hidden = false;
+        },3000);
       }
   document.body.appendChild(btnPlay.view);
 
   let styleRePlay = {
-    position: "absolute",
-    width: 100,
-    height: 50,
-    fontSize: "20px",
-    x: 120,
-    y: 30,
-    name: "REPLAY"
+      position: "absolute",
+      width: 150,
+      height: 50,
+      fontSize: "20px",
+      x: 10,
+      y: 30,
+      name: "REPLAY"
   }
   let btnReplay = new Button();
       btnReplay._initStyleElement(styleRePlay);
+      btnReplay.view.hidden = true;
       btnReplay.view.onclick = function () {
+        point = 10000;
         scoreDOM.value = "SCORE: " + point;
-        createCards();
+        isWin = 0;
+        createCards("REPLAY");
       }
   document.body.appendChild(btnReplay.view);
 
   let styleRetry = {
-    position: "absolute",
-    width: 100,
-    height: 50,
-    fontSize: "20px",
-    x: 230,
-    y: 30,
-    name: "RETRY"
+      position: "absolute",
+      width: 150,
+      height: 50,
+      fontSize: "20px",
+      x: 365,
+      y: 30,
+      name: "RETRY"
   }
   let btnRetry = new Button();
       btnRetry._initStyleElement(styleRetry);
+      btnRetry.view.hidden = true;
       btnRetry.view.onclick = function () {
-        alert("Button RETRY is clicked");
+        point = 10000;
+        scoreDOM.value = "SCORE: " + point;
+        isWin = 0;
+        createCards("RETRY");
       }
   document.body.appendChild(btnRetry.view);
 
@@ -226,5 +242,21 @@ scoreDOM.value = `SCORE: ${point}`;
           card.firstChild.style.display = "flex";
           card.lastChild.hidden = true;
     }) ;
-     tl.to(card, { scale: 1.2, duration: 0.5 });
+     tl.to(card, { scale: 1, duration: 0.5 });
+  }
+
+  function zoomCard(card) {
+    let tl = gsap.timeline({ repeat: 0, repeatDelay: 0 });
+      tl.add(function () {
+      card.firstChild.style.display = 'none';
+      card.lastChild.hidden = false;
+      card.style.zIndex = 3;
+    }) ;
+    tl.to(card, { scale: 1.2, duration: 0.5 });
+}
+
+  function detroyCard() {
+    for (let item of arrCovers)
+    item.remove();
+    arrCovers = [];
   }
